6abd8967ad2e5ff4c583247bd7092ee65ed903dd
Lenovo <seyitliyewgurban@gmail.com>
2023-09-06 10:22:03 +0300

    init

--- first/nums.py
+++ first/nums.py
@@ -1,4 +1,4 @@
-def plus(a, b):
+def plus(a : int, b : int) -> int:
     return a + b
 
 text = "new_text"
@@ -6,7 +6,12 @@ text = "new_text"
 def minus(a, b):
     return a - b
 
-a = "bar"
+b = "Gurban"
+
+def mul(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print(plus(2, 2))
     print(minus(4, 2))
+    print(mul(3, 5))
