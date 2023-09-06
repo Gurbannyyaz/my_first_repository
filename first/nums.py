1b8f5fc60803d6d267cf9592ae7616050ef75693
Lenovo <seyitliyewgurban@gmail.com>
2023-09-06 12:38:56 +0300

    taze funksiya

--- first/nums.py
+++ first/nums.py
@@ -7,7 +7,12 @@ def minus(a, b):
 def mul(a, b, c):
     return a * b * c
 
+def prosto():
+    print("yaryndan ogryn")
+
 if __name__ == "__main__":
     print(plus(2, 2))
     print(minus(4, 2))
     print(mul(3, 5, 5))
+    prosto()
+
